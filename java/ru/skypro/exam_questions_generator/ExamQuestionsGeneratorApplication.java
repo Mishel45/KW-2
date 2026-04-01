06f1b8fdaea768ec6ad4d5d73a1cefbac4dced3b
Mishel45 <mikhail-pavlov1980@mail.ru>
2026-04-01 13:15:34 +0300

    KW 2 start

---
+++ java/ru/skypro/exam_questions_generator/ExamQuestionsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.exam_questions_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionsGeneratorApplication.class, args);
+	}
+
+}
